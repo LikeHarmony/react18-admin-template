7769515e822264424971912600d10dacd263a057
dianhu.liu <liudianhu829@163.com>
2025-02-13 14:10:59 +0800

    feat: sourcemap

--- webpack.config.js
+++ webpack.config.js
@@ -65,7 +65,12 @@ module.exports = {
             ]
           }
         }
-      }
+      },
+      {
+        test: /\.js$/,
+        enforce: 'pre',
+        use: ['source-map-loader'],
+      },
     ]
   },
   resolve: {
